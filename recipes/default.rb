9208c3e3bb26a5cbbe630a92694979097e274c42
Will Koehler <wck555@gmail.com>
2018-11-08 10:20:09 -0500

    Add placeholder for OS packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# WCK this is where we can put system packages to be installed
+# node.default['packages-cookbook'] = [
+#  'nodejs',
+#  'imagemagick'
+# ]
