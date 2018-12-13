143e73635183a334c5f96efee5ea6bdd75c6c05a
Will Koehler <wck555@gmail.com>
2018-12-13 14:21:34 -0500

    Update metadata
    FYI the chef_version is not honored when updating dependencies. But setting it to 12.x anyway to document the OpsWorks requirement

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,10 @@
-name 'OpsWorksRails2'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+name 'opsworks_rails'
+maintainer 'WillKoehler'
+maintainer_email 'wck555@gmail.com'
 license 'All Rights Reserved'
-description 'Installs/Configures OpsWorksRails2'
-long_description 'Installs/Configures OpsWorksRails2'
-version '0.1.0'
-chef_version '>= 13.0'
+description 'Deploy/Manage Rails apps via OpsWorks Stacks'
+version '1.0.0'
+chef_version '~> 12'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
