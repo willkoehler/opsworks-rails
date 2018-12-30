bca2ac4a4644cde7944aeafef1755ba60acb69ae
Will Koehler <wck555@gmail.com>
2018-12-29 22:22:13 -0500

    Fix monitoring recipe to use current instance
    search(:aws_opsworks_instance) returns all the instances in the stack. search(:aws_opsworks_instance, "self:true") returns the instance the recipe is currently running on.

--- monitoring/recipes/drive_space.rb
+++ monitoring/recipes/drive_space.rb
@@ -2,7 +2,7 @@ template "/srv/monitor_drive_space" do
   source "monitor_drive_space.erb"
   owner 'root'
   mode 0744
-  variables(instance: search(:aws_opsworks_instance).first, stack: search(:aws_opsworks_stack).first)
+  variables(instance: search(:aws_opsworks_instance, "self:true").first, stack: search(:aws_opsworks_stack).first)
 end
 
 cron "monitor_drive_space" do
