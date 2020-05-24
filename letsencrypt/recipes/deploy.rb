660431232c44088946921f314075498555eea220
Will Koehler <wck555@gmail.com>
2020-05-24 18:36:06 -0400

    Cookbooks updated at 2020-05-24 22:36:05 UTC

--- letsencrypt/recipes/deploy.rb
+++ letsencrypt/recipes/deploy.rb
@@ -1,5 +1,7 @@
 Chef::Resource::User.send(:include, Helpers)
 
+valid_domains_for("a", "b", "c.d")
+
 every_enabled_application do |application|
   email = node[:lets_encrypt_notification_email]
   domains = "-d " + valid_domains_for(application).join(" -d ")
