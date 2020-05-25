1a69b7792bd2b74709b2095b0b994d1b3422c79e
Will Koehler <wck555@gmail.com>
2020-05-25 13:23:07 -0400

    Cookbooks updated at 2020-05-25 17:23:07 UTC

--- letsencrypt/recipes/deploy.rb
+++ letsencrypt/recipes/deploy.rb
@@ -2,18 +2,17 @@ every_enabled_application do |application|
   email = node[:lets_encrypt_notification_email]
   domains = "-d " + valid_domains_for(application).join(" -d ")
 
-  # Sleep to allow opsworks_ruby::deploy to finish updating Nginx config. Super hacky, but don't see any other way
-  # to wait for opsworks_ruby::deploy to finish. If we don't wait there's a race condition between the Nginx config
-  # change made by our deploy and certbot
-  chef_sleep "Wait for Nginx config to be updated..." do
-    seconds 5
-  end
+  # # Sleep to allow opsworks_ruby::deploy to finish updating Nginx config. Super hacky, but don't see any other way
+  # # to wait for opsworks_ruby::deploy to finish. If we don't wait there's a race condition between the Nginx config
+  # # change made by our deploy and certbot
+  # chef_sleep "Wait for Nginx config to be updated..." do
+  #   seconds 5
+  # end
 
   # Run certbot to setup TLS for the application
-  certbot_command = "certbot --noninteractive --nginx --agree-tos --no-eff-email --no-redirect --keep-until-expiring -m #{email} #{domains}"
-  Chef::Log.info("Running Certbot: #{certbot_command}")
-
   execute "run certbot" do
+    certbot_command = "certbot --noninteractive --nginx --agree-tos --no-eff-email --no-redirect --keep-until-expiring -m #{email} #{domains}"
+    Chef::Log.info("Running Certbot: #{certbot_command}")
     command certbot_command
     user "root"
   end
