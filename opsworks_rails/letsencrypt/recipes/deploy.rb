130b71e679859c28c91408c0409a7c6e14da938e
Will Koehler <wck555@gmail.com>
2020-08-14 16:41:30 -0400

    Cookbooks updated at 2020-08-14 20:41:30 UTC

--- opsworks_rails/letsencrypt/recipes/deploy.rb
+++ opsworks_rails/letsencrypt/recipes/deploy.rb
@@ -6,7 +6,33 @@ every_enabled_application do |application|
   # We log the full command in the execute comment vs using a Chef::Log.info because Chef::Log.info
   # runs at time of parsing while the execute block is evaluated later, based on the runlist. The
   # Chef::Log.info statement will appear in the log far ahead of when the command is actually executed.
-  certbot_command = "certbot --noninteractive --nginx --agree-tos --no-eff-email --no-redirect --keep-until-expiring -m #{email} #{domains}"
+  #
+  # The "echo | " sends a <return> (\n) to the command. This is needed when we have a wildcard cert
+  # because we get a prompt from certbot and there's no way to automate it.
+  #
+  # Which server blocks would you like to modify?
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # 1: File: /etc/nginx/sites-enabled/chronicle.conf
+  # Addresses: 443 ssl
+  # Names: rails1, *.cmscontracts.com, cmscontracts.com, chronicle
+  # HTTPS: Yes
+
+  # 2: File: /etc/nginx/sites-enabled/signyl_for_rehab.conf
+  # Addresses: 443 ssl
+  # Names: rails1, rehab.psiapps.com, signyl_for_rehab
+  # HTTPS: Yes
+
+  # 3: File: /etc/nginx/sites-enabled/signyl_for_supply_chain.conf
+  # Addresses: 443 ssl
+  # Names: signyl.psiapps.com, rails1, signyl_for_supply_chain
+  # HTTPS: Yes
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Select the appropriate numbers separated by commas and/or spaces, or leave input
+  # blank to select all options shown (Enter 'c' to cancel):
+  #
+  # NOTE: Because of this, we can't use the "--noninteractive" parameter
+
+  certbot_command = "echo | certbot -a dns-route53 -i nginx --agree-tos --no-eff-email --no-redirect --keep-until-expiring -m #{email} #{domains}"
   execute "Run Certbot: #{certbot_command}" do
     command certbot_command
     user "root"
