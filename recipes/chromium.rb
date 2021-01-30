2948af64611191710acf53d60f87bf112ff31827
Will Koehler <wck555@gmail.com>
2021-01-30 15:18:05 -0500

    Replace chromium recipe with puppeteer_support
    We don't need chromium. All we actually need are support libraries so puppeteer can download and install chromium.

--- recipes/chromium.rb
+++
@@ -1,8 +0,0 @@
-# From https://gist.github.com/pragmaticivan/cff18a70dba1fa8db8cb92e42dee82c9
-apt_repository 'ffmpeg' do
-  uri 'ppa:canonical-chromium-builds/stage'
-end
-
-package %w(software-properties-common chromium-browser) do
-  action :install
-end
---
+++ recipes/puppeteer_support.rb
@@ -0,0 +1,43 @@
+# From https://github.com/puppeteer/puppeteer/issues/3443#issuecomment-433096772
+# These are packages needed on Ubuntu 18.04 for Puppeteer. Puppeteer will automatically
+# install the needed version of Chrome
+package %w(
+  libappindicator1
+  libasound2
+  libatk1.0-0
+  libcairo2
+  libcups2
+  libdbus-1-3
+  libexpat1
+  libfontconfig1
+  libgconf-2-4
+  libgdk-pixbuf2.0-0
+  libglib2.0-0
+  libgtk-3-0
+  libgbm-dev
+  libnspr4
+  libnss3
+  libpango-1.0-0
+  libpangocairo-1.0-0
+  libx11-6
+  libx11-xcb1
+  libxcb1
+  libxcomposite1
+  libxcursor1
+  libxdamage1
+  libxext6
+  libxfixes3
+  libxi6
+  libxrandr2
+  libxrender1
+  libxss1
+  libxtst6
+  ca-certificates
+  fonts-liberation
+  gconf-service
+  lsb-release
+  wget
+  xdg-utils
+) do
+  action :install
+end
